9e2ac9776b5da94c37cf6e9e60a6209fcd5cc7ea
fight_lu <linglu456456@126.com>
2017-08-23 15:49:48 +0800

    ll

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,55 @@
+<?php namespace App\Http\Controllers;
+
+class HomeController extends Controller {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Home Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller renders your application's "dashboard" for users that
+	| are authenticated. Of course, you are free to change or remove the
+	| controller as you wish. It is just here to get your app started!
+	|
+	*/
+
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		# $this->middleware('auth');
+	}
+
+	/**
+	 * Show the application dashboard to the user.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return view('home');
+	}
+
+    // TODO: 个人风采
+    public function llphoto(){
+        echo __METHOD__.' : '.__LINE__;
+    }
+
+    // TODO: 详情信息
+    public function lldetail(){
+        echo __METHOD__.' : '.__LINE__;
+    }
+
+    // TODO: 文章
+    public function llactricle(){
+        echo __METHOD__.' : '.__LINE__;
+    }
+
+    // TODO: 时光轴
+    public function lltime(){
+        echo __METHOD__.' : '.__LINE__;
+    }
+}
